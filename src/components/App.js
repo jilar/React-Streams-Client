0b77f2c4acdbeacd614b51d2c8062278c1fcda9e
Jeffrey Ilar <jilar@petzent.com>
2019-10-29 11:15:07 -0700

    Scaffolded Stream Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {BrowserRouter, Route,Link} from 'react-router-dom';
 
-import './App.css';
+const PageOne=() =>{
+  return(
+    <div>Page One
+      <Link to= '/pagetwo'>go to page 2</Link>
+    </div>
+  )};
+
+const PageTwo=() =>{
+  return(
+      <div>Page Two
+        <Link to= '/'>go to page 1</Link>
+      </div>
+  )};
+
 
 function App() {
   return (
     <div>
+    <BrowserRouter>
+      <div>
+        <Route path='/' exact component={PageOne} />
+        <Route path='/pagetwo' component={PageTwo} />
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
