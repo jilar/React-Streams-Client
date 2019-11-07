9810bd7b642d9e2d351871f713cbf6ad1894fd6d
Jeffrey Ilar <jilar@petzent.com>
2019-11-07 14:23:24 -0800

    Connected streams to userID, added naviation link from streams list to create stream

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStreams} from '../../actions';
+import {Link} from 'react-router-dom';
 
 class StreamList extends React.Component{
   componentDidMount(){
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream){
+    if(this.props.userId === stream.userId)
+    return(
+      <div className='right floated content'>
+        <button className= 'ui button primary'>
+          EDIT
+        </button>
+        <button className= 'ui button red'>
+         DELETE
+        </button>
+      </div>
+    )
+  }
+
+  renderCreate(){
+    if (this.props.isSignedIn){
+    return(
+      <div style ={{textAlign:'right'}}>
+        <Link to='/streams/new' className='ui button primary'>Stream</Link>
+      </div>
+    )};
+  }
+
   renderList(){
     return this.props.streams.map(stream=>{
       return(
         <div className= "item" key= {stream.id}>
+          {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera'/>
           <div className='content'>
             {stream.title}
@@ -28,13 +53,18 @@ class StreamList extends React.Component{
         <div className='ui celled list'>
           {this.renderList()}
         </div>
+        {this.renderCreate()}
       </div>
   )};
 };
 
 const mapStateToProps=(state)=>{
   //Object.values takes an object and places all values inside object into array
-  return {streams: Object.values(state.streams)};
+  return {
+    streams: Object.values(state.streams),
+    userId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 }
 
 export default connect(mapStateToProps,{fetchStreams})(StreamList);
